a2439602bb924e1ee926403ff95e0c7010f3961f
Kim Miguel Sobrepena <kmpsobrepena@mymail.mapua.edu.ph>
2025-07-08 14:36:21 +0800

    Database Creation

--- api/settings.py
+++ api/settings.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sKonnectDB.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database/sKonnectDB.db")
 
 SECRET_KEY = os.getenv("SECRET_KEY", "a_very_secret_key_that_should_be_changed")
 ALGORITHM = "HS256"
