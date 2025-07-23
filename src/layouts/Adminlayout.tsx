a595c1d9ede24d0e87381518f487d203eb3b2e0d
Kim Miguel Sobrepena <kmpsobrepena@mymail.mapua.edu.ph>
2025-07-24 00:12:53 +0800

    REmove somemore unused

--- src/layouts/Adminlayout.tsx
+++ src/layouts/Adminlayout.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import styles from '../layoutstyles/AdminLayout.module.css';
 import logo from '../assets/skonnect-logo-white.png';
--- src/pages/ApiTestPage.tsx
+++ src/pages/ApiTestPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from '../pagestyles/ApiTestPage.module.css'; // This uses the same CSS as before
 
