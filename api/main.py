e32be9f465a3aeff6ac4c7b467da01687a14e158
Kim Miguel Sobrepena <kmpsobrepena@mymail.mapua.edu.ph>
2025-07-24 00:50:52 +0800

    Debhugging Cors Sh

--- api/main.py
+++ api/main.py
@@ -39,7 +39,7 @@ app = FastAPI(title="sKonnect API")
 origins = [
     "http://localhost:5173",
     "https://its122l-project.onrender.com",
-    "https://its-122-l-project-lovat.vercel.app/",  
+    "https://its-122-l-project-skonnect.vercel.app/",  
     # add deployed frontend's URL here later
 ]
                               
