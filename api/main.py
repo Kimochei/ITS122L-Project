61c0bbeba298820f0780aadfbe555f71003f27e3
Kim Miguel Sobrepena <kmpsobrepena@mymail.mapua.edu.ph>
2025-07-24 13:05:08 +0800

    sasampalin ko tong katabi ko pag gumana to

--- api/main.py
+++ api/main.py
@@ -40,8 +40,8 @@ origins = [
     "http://localhost:5173",
     "https://its122l-project.onrender.com",
     "https://its-122-l-project-git-main-skonnect.vercel.app", 
-    "https://its-122-l-project-lovat.vercel.app/",
-    "https://its-122-l-project-skonnect.vercel.app/"
+    "https://its-122-l-project-lovat.vercel.app",
+    "https://its-122-l-project-skonnect.vercel.app"
 
     # add deployed frontend's URL here later
 ]
