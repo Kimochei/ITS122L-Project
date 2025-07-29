ca30e6efe61b7e1b2f49b8490f22b3d2efbc7473
unawarespecs <cnrdegala@mymail.mapua.edu.ph>
2025-07-29 08:34:31 +0800

    chore: update dependencies and integrate Bootstrap
    - Added @fontsource/roboto and bootstrap to dependencies in package.json
    - Imported Bootstrap CSS in main.tsx for styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // Import QueryClient and QueryClientProvider
 
 const queryClient = new QueryClient();
